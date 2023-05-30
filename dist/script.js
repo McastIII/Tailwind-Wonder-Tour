69b3a4f5bfe4fac653d001bdbaf5e8b2c7f769ce
McastIII <jeancapunocapuno@gmail.com>
2023-05-30 15:33:03 +0800

    first commit

--- dist/script.js
+++ dist/script.js
@@ -3,11 +3,21 @@ const toggleMenu = document.querySelector(".toggle__menu");
 const mobileNav = document.querySelector(".header__actions");
 const header = document.querySelector(".header");
 const blurr = document.querySelector(".blur");
+
+
 toggleMenu.addEventListener("click", () => {
     toggleMenu.classList.toggle("open");
     mobileNav.classList.toggle("open");
     header.classList.toggle("open");
     blurr.classList.toggle("open");
+    
+});
+
+const three = document.querySelector(".ellipsis");
+const quote = document.querySelector(".header__details");
+three.addEventListener("click", () => {
+  three.classList.toggle("open");
+  quote.classList.toggle("open");
 });
 
 //drop down
